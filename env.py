56e539561eda76b7c68133bb995038fa8c9930c4
Zahra Hassani <raha.zahra1375@gmail.com>
2023-04-04 12:59:19 +0000

    delete everything to start again

--- env.py
+++
@@ -1,2 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = "cloudinary://998779825121629:Cixcun3Cr109Ql-XHlRFW4oEBS8@drlmtwmih"
--- profiles/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from profiles import views
-
-urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-]
