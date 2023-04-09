2f8814e5933d18d43502b152db964c25f8b3c42c
Zahra Hassani <raha.zahra1375@gmail.com>
2023-04-09 06:29:06 +0000

    Add JSON web tokens to the project and configured

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 
-os.environ['CLOUDINARY_URL']= 'cloudinary://998779825121629:Cixcun3Cr109Ql-XHlRFW4oEBS8@drlmtwmih'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://998779825121629:Cixcun3Cr109Ql-XHlRFW4oEBS8@drlmtwmih'
+os.environ['DEV'] = '1'
