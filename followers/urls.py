21e1e6bebbe6c4ea241ce6188e45070eae124e02
Zahra Hassani <raha.zahra1375@gmail.com>
2023-05-28 14:50:43 +0000

    - extra-apps

--- followers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from followers import views
-
-urlpatterns = [
-    path('follows/', views.FollowList.as_view()),
-    path('follows/<int:pk>', views.FollowDetail.as_view()),
-]
